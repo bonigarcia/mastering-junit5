b053a54142fb71a5ce2edcf45f9b3a641885027d
Boni Garcia <bgarcia@gsyc.es>
2017-04-12 16:12:38 +0200

    Included tagging-filtering-gradke project

---
+++ junit5-tagging-filtering-gradle/src/test/java/io/github/bonigarcia/NonFunctionalTest.java
@@ -0,0 +1,51 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("non-functional")
+class NonFunctionalTest {
+
+    @Test
+    @Tag("performance")
+    @Tag("load")
+    void test1() {
+        System.out.println("Non-Functional Test 1 (Performance/Load)");
+    }
+
+    @Test
+    @Tag("performance")
+    @Tag("stress")
+    void test2() {
+        System.out.println("Non-Functional Test 2 (Performance/Stress)");
+    }
+
+    @Test
+    @Tag("security")
+    void test3() {
+        System.out.println("Non-Functional Test 2 (Security)");
+    }
+
+    @Test
+    @Tag("usability")
+    void test4() {
+        System.out.println("Non-Functional Test 2 (Usability)");
+    }
+
+}
