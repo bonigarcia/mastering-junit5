bc4261ea9544251522bb06b6b03786111c5c05ca
Boni Garcia <bgarcia@gsyc.es>
2017-04-13 00:37:56 +0200

    Include dependency-injection examples

---
+++ junit5-dependency-injection/src/test/java/io/github/bonigarcia/TestInfoTest.java
@@ -0,0 +1,47 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+class TestInfoTest {
+
+    @BeforeEach
+    void init(TestInfo testInfo) {
+        String displayName = testInfo.getDisplayName();
+        System.out.printf("@BeforeEach %s %n", displayName);
+    }
+
+    @Test
+    @DisplayName("my test")
+    @Tag("my tag")
+    void test1(TestInfo testInfo) {
+        System.out.println(testInfo.getDisplayName());
+        System.out.println(testInfo.getTags());
+        System.out.println(testInfo.getTestClass());
+        System.out.println(testInfo.getTestMethod());
+    }
+
+    @Test
+    void test2() {
+    }
+
+}
