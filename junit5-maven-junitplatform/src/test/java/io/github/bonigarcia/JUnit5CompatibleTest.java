7701284cb8cefdcba53579428507b9af669247b8
Boni Garcia <bgarcia@gsyc.es>
2017-06-06 20:32:53 +0200

    Include @RunWith(JUnitPlatform.class) example in Maven project

---
+++ junit5-maven-junitplatform/src/test/java/io/github/bonigarcia/JUnit5CompatibleTest.java
@@ -0,0 +1,36 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class JUnit5CompatibleTest {
+
+	@Test
+	void myTest() {
+		String message = "1+1 should be equal to 2";
+		System.out.println(message);
+
+		assertEquals(2, 1 + 1, message);
+	}
+
+}
