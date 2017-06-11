aa7653502d9fc0a3f7cdaefa57fbf129f239cb46
Boni Garcia <bgarcia@gsyc.es>
2017-06-11 17:23:06 +0200

    Include junit5-engine-spi project

---
+++ junit5-engine-spi/src/main/java/io/github/bonigarcia/MyCustomEngine.java
@@ -0,0 +1,52 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia;
+
+import org.junit.platform.engine.EngineDiscoveryRequest;
+import org.junit.platform.engine.ExecutionRequest;
+import org.junit.platform.engine.TestDescriptor;
+import org.junit.platform.engine.TestEngine;
+import org.junit.platform.engine.UniqueId;
+import org.junit.platform.engine.support.descriptor.EngineDescriptor;
+
+public class MyCustomEngine implements TestEngine {
+
+    public static final String ENGINE_ID = "my-custom-engine";
+
+    @Override
+    public String getId() {
+        return ENGINE_ID;
+    }
+
+    @Override
+    public TestDescriptor discover(EngineDiscoveryRequest discoveryRequest,
+            UniqueId uniqueId) {
+        // Discover test(s) and return a TestDescriptor object
+        TestDescriptor testDescriptor = new EngineDescriptor(uniqueId,
+                "My test");
+        return testDescriptor;
+    }
+
+    @Override
+    public void execute(ExecutionRequest request) {
+        // Use ExecutionRequest to execute TestDescriptor
+        TestDescriptor rootTestDescriptor = request.getRootTestDescriptor();
+        request.getEngineExecutionListener()
+                .executionStarted(rootTestDescriptor);
+    }
+
+}
