1d8f2896205447e500821100b7ec75529540674c
Boni Garcia <bgarcia@gsyc.es>
2017-07-27 09:46:00 +0200

    Refactor parameterized tests with @CsvSource and @CsvFileSource

--- junit5-parameterized/src/test/java/io/github/bonigarcia/CsvFileSourceParameterizedTest.java
+++ junit5-parameterized/src/test/java/io/github/bonigarcia/CsvFileSourceParameterizedTest.java
@@ -20,15 +20,15 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-class CsvSourceParameterizedTest {
+class CsvFileSourceParameterizedTest {
 
     @ParameterizedTest
-    @CsvSource({ "hello, 1", "world, 2", "'happy, testing', 3" })
-    void testWithCsvSource(String first, int second) {
-        System.out.println("Parameterized test with (String) " + first
-                + " and (int) " + second);
+    @CsvFileSource(resources = "/input.csv")
+    void testWithCsvFileSource(String first, int second) {
+        System.out.println("Yet another parameterized test with (String) "
+                + first + " and (int) " + second);
 
         assertNotNull(first);
         assertNotEquals(0, second);
