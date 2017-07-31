cc60963cdc42dcc9cd321806f37191e8cc7a7281
Boni Garcia <bgarcia@gsyc.es>
2017-07-31 16:09:59 +0200

    Include junit5-spring project

---
+++ junit5-spring/src/test/java/io/github/bonigarcia/SimpleSpringTest.java
@@ -0,0 +1,39 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { MySpringApplication.class })
+class SimpleSpringTest {
+
+    @Autowired
+    public MessagePrinter messagePrinter;
+
+    @Test
+    public void test() {
+        assertEquals("Hello world!", messagePrinter.getMessage());
+    }
+
+}
