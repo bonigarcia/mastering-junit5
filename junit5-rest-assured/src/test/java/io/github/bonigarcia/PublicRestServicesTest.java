fc1a4f0654b73cdf197e3e5f3e877c6cd1fca07e
Boni Garcia <bgarcia@gsyc.es>
2017-08-04 01:52:41 +0200

    Include junit5-rest-assured project

---
+++ junit5-rest-assured/src/test/java/io/github/bonigarcia/PublicRestServicesTest.java
@@ -0,0 +1,46 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+public class PublicRestServicesTest {
+
+    @Test
+    void testEchoService() {
+        String title = "foo";
+        String content = "bar";
+        given().when()
+                .get("http://echo.jsontest.com/title/" + title + "/content/"
+                        + content)
+                .then().assertThat().statusCode(200)
+                .body("title", equalTo(title))
+                .body("content", equalTo(content));
+    }
+
+    @Test
+    void testCountryService() {
+        given().when()
+                .get("http://services.groupkt.com/country/get/iso2code/ES")
+                .then().assertThat().statusCode(200)
+                .body("RestResponse.result.name", equalTo("Spain"));
+    }
+
+}
