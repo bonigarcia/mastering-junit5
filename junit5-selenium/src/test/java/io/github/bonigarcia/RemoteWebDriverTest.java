649b19ad7977e3fe72073fe66a852452dc85c0ef
Boni Garcia <bgarcia@gsyc.es>
2017-08-03 14:51:55 +0200

    Include junit5-selenium project with examples using selenium-jupiter

---
+++ junit5-selenium/src/test/java/io/github/bonigarcia/RemoteWebDriverTest.java
@@ -0,0 +1,40 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+@ExtendWith(SeleniumExtension.class)
+public class RemoteWebDriverTest {
+
+    @Test
+    void testWithRemoteChrome(
+            @DriverUrl("http://localhost:4444/wd/hub") @DriverCapabilities(capability = {
+                    @Capability(name = "browserName", value = "chrome"),
+                    @Capability(name = "version", value = "59") }) RemoteWebDriver remoteChrome)
+            throws InterruptedException {
+
+        remoteChrome.get("https://bonigarcia.github.io/selenium-jupiter/");
+
+        assertTrue(remoteChrome.getTitle().contains("JUnit 5 extension"));
+    }
+
+}
