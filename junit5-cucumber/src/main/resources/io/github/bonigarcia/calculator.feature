0634c47d947474068c35dd91f87fbfd2391fe1ea
Boni Garcia <bgarcia@gsyc.es>
2017-08-04 02:49:51 +0200

    Add junit5-cucumber project

---
+++ junit5-cucumber/src/main/resources/io/github/bonigarcia/calculator.feature
@@ -0,0 +1,12 @@
+Feature: Basic Arithmetic
+
+  Background: A Calculator
+    Given a calculator I just turned on
+
+  Scenario: Addition
+    When I add 4 and 5
+    Then the result is 9
+
+  Scenario: Substraction
+    When I substract 7 to 2 
+    Then the result is 5
---
+++ junit5-cucumber/src/test/java/io/github/bonigarcia/CucumberTest.java
@@ -0,0 +1,40 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.jupiter.CucumberExtension;
+
+@CucumberOptions(plugin = { "pretty" })
+@ExtendWith(CucumberExtension.class)
+public class CucumberTest {
+
+    @TestFactory
+    public Stream<DynamicTest> runCukes(Stream<DynamicTest> scenarios) {
+        List<DynamicTest> tests = scenarios.collect(Collectors.toList());
+        return tests.stream();
+    }
+
+}
