68cb3c6e627e333365cc52d72fc67cc8054bc9df
Boni Garcia <bgarcia@gsyc.es>
2017-09-16 21:24:53 +0200

    Using @AutoConfigureMockMvc for MockMcv in spring-boot-web project

--- junit5-spring-boot-web/src/test/java/io/github/bonigarcia/IndexTest.java
+++ junit5-spring-boot-web/src/test/java/io/github/bonigarcia/IndexTest.java
@@ -21,29 +21,21 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class IndexTest {
 
     @Autowired
-    WebApplicationContext webContext;
-
     MockMvc mockMvc;
-
-    @BeforeEach
-    void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webContext).build();
-    }
 
     @Test
     void testIndex() throws Exception {
--- junit5-spring-boot-web/src/test/java/io/github/bonigarcia/RedirectTest.java
+++ junit5-spring-boot-web/src/test/java/io/github/bonigarcia/RedirectTest.java
@@ -21,33 +21,25 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class RedirectTest {
-
-    @Autowired
-    WebApplicationContext webContext;
 
     @MockBean
     PageService pageService;
 
+    @Autowired
     MockMvc mockMvc;
-
-    @BeforeEach
-    void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webContext).build();
-    }
 
     @Test
     void test() throws Exception {
