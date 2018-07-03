18a0d96633f79e90d4b5204a7691e7245b876940
Boni Garcia <bgarcia@gsyc.es>
2018-07-03 16:11:52 +0200

    Include test using JUnit 5's @DisabledOnOs

---
+++ junit5-conditional-tests/src/test/java/io/github/bonigarcia/BuiltinDisabledOnOsTest.java
@@ -0,0 +1,46 @@
+/*
+ * (C) Copyright 2018 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia;
+
+import static org.junit.jupiter.api.condition.OS.LINUX;
+import static org.junit.jupiter.api.condition.OS.MAC;
+import static org.junit.jupiter.api.condition.OS.WINDOWS;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+
+class BuiltinDisabledOnOsTest {
+
+    @DisabledOnOs(LINUX)
+    @Test
+    void notLinuxTest() {
+        System.out.println("Disabled on Linux");
+    }
+
+    @DisabledOnOs(WINDOWS)
+    @Test
+    void notWinTest() {
+        System.out.println("Disabled on Windows");
+    }
+
+    @DisabledOnOs(MAC)
+    @Test
+    void notMacTest() {
+        System.out.println("Disabled on Mac");
+    }
+
+}
