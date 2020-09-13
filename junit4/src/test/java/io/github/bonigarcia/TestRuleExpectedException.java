2653ec893488852b8571c4e9ce619ebf51f6e358
Boni García <boni.garcia@uc3m.es>
2020-09-13 15:58:17 +0200

    Remove JUnit 4 rule ExpectedException.none() (deprecated in version 4.13)

--- junit4/src/test/java/io/github/bonigarcia/TestRuleExpectedException.java
+++ junit4/src/test/java/io/github/bonigarcia/TestRuleExpectedException.java
@@ -16,23 +16,17 @@
  */
 package io.github.bonigarcia;
 
-import org.junit.Rule;
+import static org.junit.Assert.assertThrows;
+
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class TestRuleExpectedException {
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    @Test
-    public void throwsNothing() {
-    }
-
     @Test
     public void throwsNullPointerException() {
-        thrown.expect(NullPointerException.class);
-        throw new NullPointerException();
+        assertThrows(NullPointerException.class, () -> {
+            throw new NullPointerException();
+        });
     }
 
 }
