a35e31a72a742e11f3e95d3e0b6bb4f1e0f2c298
Boni García <boni.garcia@uc3m.es>
2020-09-14 09:37:44 +0200

    Include examples for @EnabledForJreRange and @DisabledForJreRange (execution conditions)

---
+++ junit5-conditional-tests/src/test/java/io/github/bonigarcia/DisabledOnJreRangeTest.java
@@ -0,0 +1,91 @@
+/*
+ * (C) Copyright 2020 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.junit.jupiter.api.condition.JRE.JAVA_8;
+import static org.junit.jupiter.api.condition.JRE.JAVA_9;
+import static org.junit.jupiter.api.condition.JRE.JAVA_10;
+import static org.junit.jupiter.api.condition.JRE.JAVA_11;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledForJreRange;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.junit.jupiter.api.condition.EnabledForJreRange;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.slf4j.Logger;
+
+class DisabledOnJreRangeTest {
+
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    @Test
+    @EnabledOnJre(JAVA_8)
+    void onlyOnJava8() {
+        log.debug("This test is executed only for JRE 8");
+    }
+
+    @Test
+    @EnabledOnJre({ JAVA_9, JAVA_10 })
+    void onJava9Or10() {
+        log.debug("This test is executed only for JRE 9 or 10 ");
+    }
+
+    @Test
+    @EnabledForJreRange(min = JAVA_9, max = JAVA_11)
+    void fromJava9to11() {
+        log.debug("This test is executed only for JRE 9 to 11");
+    }
+
+    @Test
+    @EnabledForJreRange(min = JAVA_9)
+    void fromJava9toCurrentJavaFeatureNumber() {
+        log.debug("This test is executed only for JRE 9+");
+    }
+
+    @Test
+    @EnabledForJreRange(max = JAVA_11)
+    void fromJava8To11() {
+        log.debug("This test is executed only for JRE 8 to 11");
+    }
+
+    @Test
+    @DisabledOnJre(JAVA_9)
+    void notOnJava9() {
+        log.debug("This test is executed only for JRE != 9");
+    }
+
+    @Test
+    @DisabledForJreRange(min = JAVA_9, max = JAVA_11)
+    void notFromJava9to11() {
+        log.debug("This test is executed only for JRE != 9 to 11");
+    }
+
+    @Test
+    @DisabledForJreRange(min = JAVA_9)
+    void notFromJava9toCurrentJavaFeatureNumber() {
+        log.debug("This test is executed only for JRE < 9");
+    }
+
+    @Test
+    @DisabledForJreRange(max = JAVA_11)
+    void notFromJava8to11() {
+        log.debug("This test is executed only for JRE != 8 to 11");
+    }
+
+}
