fb4579eb311ae1062c11a20800f15b8918cc3fb0
Boni García <boni.garcia@uc3m.es>
2020-09-14 10:15:01 +0200

    Include @Isolated test in junit5-parallel-execution project

---
+++ junit5-parallel-execution/src/test/java/io/github/bonigarcia/IsolatedTest.java
@@ -0,0 +1,48 @@
+/*
+ * (C) Copyright 2020 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Isolated;
+import org.slf4j.Logger;
+
+@Isolated
+class IsolatedTest {
+
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    @Test
+    void test01() {
+        log.debug("IsolatedTest 1.1");
+        log.debug("IsolatedTest 1.2");
+    }
+
+    @Test
+    void test02() {
+        log.debug("IsolatedTest 2.1");
+        log.debug("IsolatedTest 2.2");
+    }
+
+    @Test
+    void test03() {
+        log.debug("IsolatedTest 3.1");
+        log.debug("IsolatedTest 3.2");
+    }
+}
