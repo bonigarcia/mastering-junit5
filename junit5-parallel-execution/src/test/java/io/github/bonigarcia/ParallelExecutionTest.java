a3444d5c2534018b3c2e68c1d981c8253f2f4fae
Boni García <boni.garcia@uc3m.es>
2020-09-16 16:09:38 +0200

    Include test using @Execution(CONCURRENT)

---
+++ junit5-parallel-execution/src/test/java/io/github/bonigarcia/ParallelExecutionTest.java
@@ -0,0 +1,49 @@
+/*
+ * (C) Copyright 2020 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.slf4j.Logger;
+
+@Disabled
+@Execution(CONCURRENT)
+class ParallelExecutionTest {
+
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    @Test
+    void test01() {
+        log.debug("test01");
+    }
+
+    @Test
+    void test02() {
+        log.debug("test02");
+    }
+
+    @Test
+    void test03() {
+        log.debug("test03");
+    }
+
+}
