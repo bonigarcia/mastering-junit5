86cea605398715673a0787607afa171df59d07aa
Boni García <boni.garcia@uc3m.es>
2020-10-03 15:39:47 +0200

    Include project junit5-cucumber-selenium

---
+++ junit5-cucumber-selenium/src/test/java/io/github/bonigarcia/SearchSteps.java
@@ -0,0 +1,65 @@
+/*
+ * (C) Copyright 2020 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia;
+
+import java.util.NoSuchElementException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SearchSteps {
+
+    private WebDriver webDriver;
+
+    @When("I navigate to {string}")
+    public void iNavigateTo(String url) {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.get(url);
+    }
+
+    @And("I type {string}")
+    public void iType(String searchQuery) {
+        webDriver.findElement(By.name("q")).sendKeys(searchQuery);
+    }
+
+    @And("I press Enter")
+    public void iPressEnter() {
+        webDriver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+    }
+
+    @Then("I should be shown results including {string}")
+    public void iShouldBeShownResultsIncluding(String result) {
+        try {
+            webDriver.findElement(
+                    By.xpath("//*[contains(text(), '" + result + "')]"));
+        } catch (NoSuchElementException e) {
+            throw new AssertionError(
+                    "\"" + result + "\" not available in results");
+        } finally {
+            webDriver.quit();
+        }
+    }
+
+}
