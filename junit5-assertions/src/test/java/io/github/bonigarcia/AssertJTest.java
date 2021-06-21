3d988a52a4dee4d9ab103ddd55042ef6dc25b989
Boni García <boni.garcia@uc3m.es>
2021-06-21 20:06:11 +0200

    Include example using AssertJ as assertion library

---
+++ junit5-assertions/src/test/java/io/github/bonigarcia/AssertJTest.java
@@ -0,0 +1,40 @@
+/*
+ * (C) Copyright 2020 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+
+public class AssertJTest {
+
+    final Logger log = getLogger(lookup().lookupClass());
+
+    @Test
+    void test() {
+        log.debug("Assertions using AssertJ");
+
+        int sum = 1 + 1;
+
+        assertThat(sum).isGreaterThan(1).isLessThan(3);
+        assertThat(sum).isEqualTo(2);
+    }
+
+}
