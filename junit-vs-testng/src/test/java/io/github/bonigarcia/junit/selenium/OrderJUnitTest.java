19cd579d5ee2d99cd700a440ad1f4e5580224d72
Boni Garcia <boni.garcia@uc3m.es>
2025-08-26 16:36:49 +0200

    Include order tests

---
+++ junit-vs-testng/src/test/java/io/github/bonigarcia/junit/selenium/OrderJUnitTest.java
@@ -0,0 +1,76 @@
+/*
+ * (C) Copyright 2025 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.junit.selenium;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@TestMethodOrder(OrderAnnotation.class)
+class OrderJUnitTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterAll
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    @Order(1)
+    void testA() {
+        driver.get(
+                "https://bonigarcia.dev/selenium-webdriver-java/navigation1.html");
+        assertBodyContains("Lorem ipsum");
+    }
+
+    @Test
+    @Order(2)
+    void testB() {
+        driver.findElement(By.linkText("2")).click();
+        assertBodyContains("Ut enim");
+    }
+
+    @Test
+    @Order(3)
+    void testC() {
+        driver.findElement(By.linkText("3")).click();
+        assertBodyContains("Excepteur sint");
+    }
+
+    void assertBodyContains(String text) {
+        String bodyText = driver.findElement(By.tagName("body")).getText();
+        assertThat(bodyText).contains(text);
+    }
+
+}
