a8a58e69c587006839ebf7976e533fc0b0aeda32
Boni Garcia <boni.garcia@uc3m.es>
2025-08-27 16:00:45 +0200

    Include disabled and conditional tests

---
+++ junit-vs-testng/src/test/java/io/github/bonigarcia/junit/basic/DisabledJupiterTest.java
@@ -0,0 +1,47 @@
+/*
+ * (C) Copyright 2025 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.junit.basic;
+
+import static org.junit.jupiter.api.condition.JRE.JAVA_8;
+import static org.junit.jupiter.api.condition.OS.MAC;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+
+class DisabledJupiterTest {
+
+    @Disabled("Optional reason for disabling")
+    @Test
+    public void testDisabled1() {
+        // Test logic
+    }
+
+    @DisabledOnJre(JAVA_8)
+    @Test
+    public void testDisabled2() {
+        // Test logic
+    }
+
+    @EnabledOnOs(MAC)
+    @Test
+    public void testDisabled3() {
+        // Test logic
+    }
+
+}
