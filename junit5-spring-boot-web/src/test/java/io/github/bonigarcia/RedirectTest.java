32642983d6a2c2c63add5a483ac78c9b9a67d0f2
Boni Garcia <boni.garcia@uc3m.es>
2026-07-13 14:28:51 +0200

    Fix Spring Boot projects

--- junit5-spring-boot-web/src/test/java/io/github/bonigarcia/RedirectTest.java
+++ junit5-spring-boot-web/src/test/java/io/github/bonigarcia/RedirectTest.java
@@ -24,9 +24,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -35,7 +35,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 class RedirectTest {
 
-    @MockBean
+    @MockitoBean
     PageService pageService;
 
     @Autowired
